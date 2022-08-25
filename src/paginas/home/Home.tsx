c2784404a31bdb413daf0392eb0c52d0035c0161
melissaJll <melissa.lequipe@gmail.com>
2022-08-25 16:54:25 -0300

    Upload do Front-end do projeto blog pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <p>Texto</p>
+        </>
+    );
+}
+
+export default Home;
