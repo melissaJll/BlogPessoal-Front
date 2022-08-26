e12eff8f1d5ff994d09102f7754eea30ac559198
melissaJll <melissa.lequipe@gmail.com>
2022-08-26 17:54:50 -0300

    Menu e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home/>
+      <Footer/>
+    </>
   );
 }
 
