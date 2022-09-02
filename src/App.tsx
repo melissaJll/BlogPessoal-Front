755faad06620d555f745a899a7d16e5397c4e46a
melissaJll <melissa.lequipe@gmail.com>
2022-09-01 21:02:23 -0300

    Adicionando mudanças no css e pastas model e service

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import SobreNos from './paginas/sobrenos/SobreNos';
+import Sobre from './paginas/sobrenos/SobreNos';
 import './App.css';
 import Login  from './paginas/login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
 
 function App() {
   return (
@@ -13,9 +16,10 @@ function App() {
           <div style={{ minHeight: '100vh'}}>
           <Routes> /
             <Route path="/" element={<Login />} />
+            <Route path="/sobrenos" element={<SobreNos />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
           </Routes>
           </div>
         <Footer />
